ed6197c0b6515fbec0afb107e3f4cb0e93034197
Diego1809 <diegoa-sandovalf@unilibre.edu.co>
2021-08-30 06:58:35 -0500

    modificaciones App.vue y Singin.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,38 @@
 <template>
   <div id="app">
     <!-- Header -->
-    <nav class="navbar navbar-expand-lg justify-content-between">
+    <nav class="navbar navbar-dark navbar-expand-lg justify-content-between">
       <a class="navbar-brand" href="#"> <h1>Mintic To Do</h1></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#"
-              >Usuarios <span class="sr-only">(current)</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Tareas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Categorías</a>
-          </li>
-        </ul>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div class="btn-group" role="group" aria-label="Toggle navigation">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <button type="button" class="btn">Calendar</button>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn">Task</button>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn">Category</button>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn" >Log out</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-    <!-- Header -->
-
-
-    <!-- Footer -->
+    <div class=container-fluid>
+      <router-view />
+    </div>
     
-    <router-view />
+    <!-- Footer -->
+    <nav class="navbar navbar-expand-lg justify-content-center">
+      <a class="navbar-brand" href="#">DW1_CP4_G4</a>
+    </nav>
   </div>
 </template>
 
@@ -48,11 +44,27 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.navbar{
+  background-color: #5264ef;
+}
+.navbar .navbar-brand{
+  color: #fff;
+}
+.navbar-nav .nav-item .btn{
+  background: #3e4b83;
+  color: #ffffff;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  text-align: center;
+  margin: 0px 10px;
+  width: 100px;
+}
+.container-fluid{
+  min-height: auto;
 }
 </style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,15 +1,10 @@
 <template>
   <section class="vh-100">
-    <div class="container py-5 h-100">
+    <div class="container h-100">
       <div class="row d-flex align-items-center justify-content-center h-100">
         <div class="col-md-8 col-lg-7 col-xl-6">
-          <img
-            src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-            class="img-fluid"
-            alt="Phone image"
-          />
+          <img src="..\assets\Mobile note list-amico.svg" class="img-fluid" alt="Phone image"/>
         </div>
-
         <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
           <ul class="nav nav-tabs">
             <li class="nav-item">
@@ -21,6 +16,8 @@
           </ul>
           <div class="tab-content">
             <div id="signIn" class="container tab-pane active">
+              <br />
+              <h2>Welcome</h2>
               <br />
               <form>
                 <!-- Email input -->
@@ -66,13 +63,15 @@
                 </div>
 
                 <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-lg btn-block">
+                <button type="submit" class="btn btn-lg btn-block">
                   Sign in
                 </button>
               </form>
             </div>
-            <div id="signup" class="container tab-pane active">
+            <div id="signup" class="container tab-pane">
               <br />
+              <h2>Creat your account</h2>
+              <br/>
               <form>
                 <!-- Email input -->
                 <div class="form-outline mb-4">
@@ -103,8 +102,8 @@
                 ></div>
 
                 <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-lg btn-block">
-                  Sign in
+                <button type="submit" class="btn btn-lg btn-block">
+                  Sign up
                 </button>
               </form>
             </div>
@@ -131,6 +130,7 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+  color: #5264ef;
 }
 ul {
   list-style-type: none;
@@ -140,7 +140,18 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
+.container{
+  padding-bottom: 1vh;
+}
+.nav-item .nav-link{
   color: #42b983;
+}
+.nav-item .nav-link.active{
+  background-color: #edfaff;
+  border-bottom-color: #edfaff;
+}
+.btn{
+  background-color: #5264ef;
+  color: #fff;
 }
 </style>
